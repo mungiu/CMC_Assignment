210b18032cd9777ed343b178fae930bb7920465a
Andrei Mungiu <andrei_mungiu@yahoo.com>
2020-10-14 21:04:52 +0300

    parse MVP finalized
    Parse with AST tree should be built next

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -4,10 +4,7 @@ import static com.company.TokenKind.*;
 
 public class Parser {
     private Scanner scan;
-
-
     private Token currentTerminal;
-
 
     public Parser(Scanner scan) {
         this.scan = scan;
@@ -15,7 +12,6 @@ public class Parser {
         currentTerminal = scan.scan();
     }
 
-    // 1st layer
     public void parseProgram() {
         parseCommandList();
 
@@ -23,8 +19,15 @@ public class Parser {
             System.out.println("Tokens found after end of program");
     }
 
-
     private void parseCommandList() {
+        parseCommand();
+        while (currentTerminal.kind == SEMICOLON) {
+            accept(SEMICOLON);
+            parseCommand();
+        }
+    }
+
+    private void parseCommand() {
         // while source document is not yet finished
         while (currentTerminal.kind != EOT) {
             switch (currentTerminal.kind) {
@@ -32,197 +35,55 @@ public class Parser {
                     accept(INT);
                     accept(IDENTIFIER);
                     accept(LEFT_SQUARE_BRACKET);
-                    accept(NUMBERS);
+                    parseExpressionComponent();
                     accept(RIGHT_SQUARE_BRACKET);
                     break;
                 case CHAR:
                     accept(CHAR);
                     accept(IDENTIFIER);
                     accept(LEFT_SQUARE_BRACKET);
-                    accept(NUMBERS);
+                    parseCharacter();
                     accept(RIGHT_SQUARE_BRACKET);
                     break;
                 case ARRAY:
                     accept(ARRAY);
                     accept(IDENTIFIER);
                     accept(LEFT_SQUARE_BRACKET);
-                    accept(NUMBERS);
+                    parseExpressionList();
                     accept(RIGHT_SQUARE_BRACKET);
+                    break;
+                case FUNC:
+                    accept(FUNC);
+                    parseExpressionComponent();
+                    accept(LEFT_SQUARE_BRACKET);
+                    parseCommandList();
+                    accept(RIGHT_SQUARE_BRACKET);
+                    break;
+                case RETURN:
+                    accept(RETURN);
+                    parseExpression();
                     break;
                 case WHILE:
                     accept(WHILE);
-                    parseExpressionList();
+                    parseExpression();
                     accept(LEFT_SQUARE_BRACKET);
                     parseCommandList();
                     accept(RIGHT_SQUARE_BRACKET);
                     break;
                 case IF:
-                    parseExpressionList();
+                    accept(IF);
+                    parseExpression();
                     accept(LEFT_SQUARE_BRACKET);
                     parseCommandList();
                     accept(RIGHT_SQUARE_BRACKET);
-                case IDENTIFIER:            // CASE METHOD
-                    accept(IDENTIFIER);
-                    accept(LEFT_PARANTHESIS);
-                    parseVariableList();
-                    accept(RIGHT_PARANTHESIS);
-                    switch (currentTerminal.kind) {
-                        case SEMICOLON:             // CASE METHOD CALL
-                            break;
-                        case LEFT_SQUARE_BRACKET:   // CASE METHOD DECLARATION
-                            accept(LEFT_SQUARE_BRACKET);
-                            parseCommandList();
-                            accept(RIGHT_SQUARE_BRACKET);
-                            break;
-                    }
-
+                default:
+                    parseExpressionComponent();
             }
 
             // the ; at the end of each command
             accept(SEMICOLON);
         }
     }
-
-
-    private void parseDeclarations() {
-        while (currentTerminal.kind == VAR ||
-                currentTerminal.kind == FUNC)
-            parseOneDeclaration();
-    }
-
-
-    private void parseOneDeclaration() {
-        switch (currentTerminal.kind) {
-            case VAR:
-                accept(VAR);
-                accept(IDENTIFIER);
-                accept(SEMICOLON);
-                break;
-
-            case FUNC:
-                accept(FUNC);
-                accept(IDENTIFIER);
-                accept(LEFT_PARANTHESIS);
-
-                if (currentTerminal.kind == IDENTIFIER)
-                    parseIdList();
-
-                accept(RIGHT_PARANTHESIS);
-                parseCommandList();
-                accept(RETURN);
-                parseExpression();
-                accept(SEMICOLON);
-                break;
-
-            default:
-                System.out.println("var or func expected");
-                break;
-        }
-    }
-
-    private void parseIdList() {
-        accept(IDENTIFIER);
-
-        while (currentTerminal.kind == COMMA) {
-            accept(COMMA);
-            accept(IDENTIFIER);
-        }
-    }
-
-    private void parseOneStatement() {
-        switch (currentTerminal.kind) {
-            case IDENTIFIER:
-            case NUMBERS:
-            case OPERATOR:
-            case LEFT_SQUARE_BRACKET:
-                parseExpression();
-                accept(RIGH_SQUARE_BRACKET);
-                break;
-
-            case IF:
-                accept(IF);
-                parseExpression();
-                accept(LEFT_SQUARE_BRACKET);
-                parseStatements();
-
-                if (currentTerminal.kind == IF) {
-                    accept(IF);
-                    parseStatements();
-                }
-
-                accept(RIGHT_SQUARE_BRACKET);
-//				accept( SEMICOLON );
-                break;
-
-            case WHILE:
-                accept(WHILE);
-                parseExpression();
-                parseStatements();
-//				accept( SEMICOLON );
-                break;
-
-            case SAY:
-                accept(SAY);
-                parseExpression();
-                accept(SEMICOLON);
-                break;
-
-            default:
-                System.out.println("Error in statement");
-                break;
-        }
-    }
-
-    private void parseStatements() {
-        while (currentTerminal.kind == IDENTIFIER ||
-                currentTerminal.kind == OPERATOR ||
-                currentTerminal.kind == NUMBERS ||
-                currentTerminal.kind == LEFT_SQUARE_BRACKET ||
-                currentTerminal.kind == IF ||
-                currentTerminal.kind == WHILE ||
-                currentTerminal.kind == SAY)
-            parseOneStatement();
-    }
-
-    private void parsePrimary() {
-        switch (currentTerminal.kind) {
-            case IDENTIFIER:
-                accept(IDENTIFIER);
-
-                if (currentTerminal.kind == LEFT_SQUARE_BRACKET) {
-                    accept(LEFT_SQUARE_BRACKET);
-
-                    if (currentTerminal.kind == IDENTIFIER ||
-                            currentTerminal.kind == NUMBERS ||
-                            currentTerminal.kind == OPERATOR)
-                        parseExpressionList();
-
-
-                    accept(RIGHT_SQUARE_BRACKET);
-                }
-                break;
-
-            case NUMBERS:
-                accept(NUMBERS);
-                break;
-
-            case OPERATOR:
-                accept(OPERATOR);
-                parsePrimary();
-                break;
-
-            case LEFT_SQUARE_BRACKET:
-                accept(LEFT_SQUARE_BRACKET);
-                parseExpression();
-                accept(RIGHT_SQUARE_BRACKET);
-                break;
-
-            default:
-                System.out.println("Error in primary");
-                break;
-        }
-    }
-
 
     private void parseExpressionList() {
         parseExpression();
@@ -233,11 +94,50 @@ public class Parser {
     }
 
     private void parseExpression() {
-        parsePrimary();
+        parseExpressionComponent();
+
         while (currentTerminal.kind == OPERATOR) {
             accept(OPERATOR);
-            parsePrimary();
+            parseExpressionComponent();
         }
+    }
+
+    private void parseExpressionComponent() {
+        switch (currentTerminal.kind) {
+            case NUMBERS:
+                accept(NUMBERS);
+                break;
+            case OPERATOR:
+                accept(OPERATOR);
+                parseExpressionComponent();
+                break;
+            case IDENTIFIER:
+                accept(IDENTIFIER);
+                if (currentTerminal.kind == LEFT_PARANTHESIS) {
+                    accept(LEFT_PARANTHESIS);
+                    if (currentTerminal.kind == NUMBERS
+                            || currentTerminal.kind == OPERATOR
+                            || currentTerminal.kind == IDENTIFIER
+                            || currentTerminal.kind == LEFT_PARANTHESIS
+                            || currentTerminal.kind == APOSTROPHE) {
+                        parseExpressionList();
+                    }
+                    accept(RIGHT_PARANTHESIS);
+                }
+
+                break;
+            case APOSTROPHE:
+                parseCharacter();
+            default:
+                System.out.println("Error in ExpressionComponent");
+                break;
+        }
+    }
+
+    private void parseCharacter() {
+        accept(APOSTROPHE);
+        accept(IDENTIFIER);
+        accept(APOSTROPHE);
     }
 
     /**
--- src/com/company/TokenKind.java
+++ src/com/company/TokenKind.java
@@ -6,65 +6,62 @@
 package com.company;
 
 
-public enum TokenKind
-{
-	// INVESTIGATED BY JEPPE
-	// Calls empty constructor of TokenKind
-	IDENTIFIER,
-	NUMBERS,
-	OPERATOR,
+public enum TokenKind {
+    // INVESTIGATED BY JEPPE
+    // Calls empty constructor of TokenKind
+    IDENTIFIER,
+    NUMBERS,
+    OPERATOR,
 
-	// INVESTIGATED BY ANDREI
-	// Calls TokenKind constructor that takes a string (NOTE THIS IS HARDCODED INSTANTIATION)
-	//Variable types
-	CHAR("Char"),
-	INT("Int"),
-	ARRAY("Array"),
-	WHILE( "&" ),
-	IF( "if" ),
+    // INVESTIGATED BY ANDREI
+    // Calls TokenKind constructor that takes a string (NOTE THIS IS HARDCODED INSTANTIATION)
+    //Variable types
+    CHAR("Char"),
+    INT("Int"),
+    ARRAY("Array"),
+    WHILE("&"),
+    IF("if"),
+    FUNC("Func"),
 
-	QUOTE("`"),
-	RETURN( "return" ),
-	SAY( "say" ),
-	THEN( "then" ),
-	VAR( "var" ),
-	
+    QUOTE("`"),
+    RETURN("return"),
+    SAY("say"),
+    THEN("then"),
+    VAR("var"),
 
-	APOSTROPHE("\'"),
-	LEFTBRACKET("{"),
-	RIGHTBRACKET("}"),
-	LEFT_SQUARE_BRACKET("["),
-	RIGHT_SQUARE_BRACKET("]"),
-	LEFT_PARANTHESIS( "(" ),
-	RIGHT_PARANTHESIS( ")" ),
-	SEMICOLON( ";" ),
-	COMMA( "," ),
-	EQUALSIGN("="),
 
-	
-	EOT,
-	
-	ERROR;
-	
-	// spelling is set when the ENUM constructor is called ABOVE
-	private String spelling = null;
-	
-	
-	private TokenKind()
-	{
-	}
-	
-	
-	private TokenKind( String spelling )
-	{
-		this.spelling = spelling;
-	}
-	
-	
-	public String getSpelling()
-	{
-		return spelling;
-	}
+    APOSTROPHE("\'"),
+    LEFTBRACKET("{"),
+    RIGHTBRACKET("}"),
+    LEFT_SQUARE_BRACKET("["),
+    RIGHT_SQUARE_BRACKET("]"),
+    LEFT_PARANTHESIS("("),
+    RIGHT_PARANTHESIS(")"),
+    SEMICOLON(";"),
+    COMMA(","),
+    EQUALSIGN("="),
+    SPACE(" "),
+
+    EOT,
+
+    ERROR;
+
+    // spelling is set when the ENUM constructor is called ABOVE
+    private String spelling = null;
+
+
+    private TokenKind() {
+    }
+
+
+    private TokenKind(String spelling) {
+        this.spelling = spelling;
+    }
+
+
+    public String getSpelling() {
+        return spelling;
+    }
 }
 
 
